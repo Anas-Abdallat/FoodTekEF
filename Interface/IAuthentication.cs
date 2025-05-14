557e7c0e15640b96e1444d2876466ba7ecbb913f
FaeyzSalaheen <faez.alabadi@gmail.com>
2025-05-14 11:43:48 +0300

    Implement SignUp

--- Interface/IAuthentication.cs
+++ Interface/IAuthentication.cs
@@ -1,13 +1,15 @@
-﻿using FoodTekEF.DTOs.Login_DTOs;
+﻿using FoodTekEF.DTOs;
+using FoodTekEF.DTOs.Login_DTOs;
 using FoodTekEF.DTOs.SignUp_DTO;
 
 namespace FoodTekEF.Interface
 {
-    public interface  Authentication
+    public interface IAuthentication
     {
         Task<string> SignUp(SignUp input);
         Task<string> Login(LoginRequest input);
-        Task<string> sendOTP();
+        Task <bool> Restpassword(Restpassword input);
+        Task<string> SendOTP(string Email);
 
     }
 }
---
+++ Service/AuthServise.cs
@@ -0,0 +1,58 @@
+﻿using FoodTekEF.Context;
+using FoodTekEF.DTOs;
+using FoodTekEF.DTOs.Login_DTOs;
+using FoodTekEF.DTOs.SignUp_DTO;
+using FoodTekEF.Entities;
+using FoodTekEF.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodTekEF.Service
+{
+    public class AuthServise : IAuthentication
+    {
+        private readonly FoodTekDbContext _context;
+        public AuthServise(FoodTekDbContext context)
+        {
+            _context = context;
+        }
+        public Task<string> Login(LoginRequest input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Restpassword(Restpassword input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SendOTP(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SignUp(SignUp input)
+        {
+            User user = new User();
+            user.Fullname = input.Name;
+            user.Email = input.Email;
+            user.Password = input.Password;
+            user.PhoneNumber = input.PhoneNumber;
+            user.Birthdate = input.Birthdate;
+            user.JoinDate = DateTime.Now;
+            user.IsActive = true;
+            user.CreatedBy = ("system");
+            user.CreatedAt = DateTime.Now;
+            user.UpdatedBy = ("system");
+            user.UpdatedAt = DateTime.Now;
+            user.Role =4 ;
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Task.FromResult("User Created Successfully");
+
+
+
+        }
+    }
+
+
+}
